acdd80f286c759ccfa75bbc94146daff3b580465
X <x@Mac-mini-X.local>
2024-03-18 13:00:20 +0300

    fix create class props

--- src/provider.php
+++ src/provider.php
@@ -4,52 +4,37 @@ namespace marksync\provider;
 
 trait provider
 {
+    private $___binding___ = [];
+
     /**
      * @xdebug_never
      */
     public function __get($alias){
-        if (!property_exists($this, $alias)) 
-            $this->$alias = $this->__getAlias__($alias);
+        if (!isset($this->___binding___[$alias]))
+            $this->___binding___[$alias] = $this->__getAlias__($alias);
 
-        return $this->$alias;
+        return $this->___binding___[$alias];
     }
 
-    private function __getAlias__(string $alias){
+
+    private function __getAlias__(string $alias)
+    {
         if (method_exists($this, "_$alias"))
-            return $this->___bind($alias, $this->{"_$alias"}());
+            return $this->{"_$alias"}();
 
         if (method_exists($this, $alias))
             if (Container::isset($alias))
-                return $this->___bind($alias, Container::get($alias));
+                return Container::get($alias);
             else
-                return $this->___bind($alias, Container::set($alias, $this->$alias()));
+                return Container::set($alias, $this->$alias());
         else
             if (method_exists($this, '___get')) 
-                return $this->___bind($alias, $this->___get($alias));
+                return $this->___get($alias);
     }
 
-    private function ___bind($alias, $element){
-        if (!isset($this->___binding___))
-            $this->___binding___ = [];
-        $this->___binding___[] = $alias;        
-
-        return $element;
-    }
-
-    private function ___clearBindings(){
-        foreach ($this->___binding___ as $prop) {
-            unset($this->$prop);
-        }
-        $this->___binding___ = [];
-    }
 
     private function super(string $alias){
         return fn($class) => Container::set($alias, $class);
     }
 
-    protected function ___playground(string $name, callable $run){
-        $this->___clearBindings();
-        Container::runNamespace($name, $run);
-        $this->___clearBindings();
-    }
 }
