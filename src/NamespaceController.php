89dfad52721a289b7bcdf166dc201833cba1e992
Picus <picus.2022@mail.ru>
2024-05-29 12:13:29 +0300

    sn controller

---
+++ src/NamespaceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace marksync\provider;
+
+
+class NamespaceController
+{
+
+
+
+    function __construct(private string $root, private string $namespace)
+    {
+        
+    }
+
+    function handle($file, $namespace)
+    {
+        
+    }
+}
